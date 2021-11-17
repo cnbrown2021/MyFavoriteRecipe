f6c11b04bc7614876134d4204d7f62326ffb1a30
Ciara Brown <cnbrown2021@gmail.com>
2021-11-17 08:35:55 -0500

    Add project files.

---
+++ MyFavoriteRecipe.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFavoriteRecipe.WebMVC.MvcApplication" Language="C#" %>
---
+++ MyFavoriteRecipe.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFavoriteRecipe.WebMVC.Startup))]
+namespace MyFavoriteRecipe.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
