21aa8ed2eabc2143cf455214a56964c86208f039
Ciara Brown <cnbrown2021@gmail.com>
2021-11-17 19:21:57 -0500

    MealCategory Service

--- MyFavoriteRecipe.Services/MealCategoryService.cs
+++ MyFavoriteRecipe.Services/MealCategoryService.cs
@@ -1,4 +1,6 @@
-﻿using MyFavoriteRecipes.Model;
+﻿using MyFavoriteRecipe.WebMVC.Models;
+using MyFavoriteRecipes.Data;
+using MyFavoriteRecipes.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,35 @@ namespace MyFavoriteRecipe.Services
     {
         // Create
         public bool CreateMealCategory(MealCategoryCreate category)
+        {
+            var content = new MealCategory()
+            {
+                CategoryName = category.CategoryName,
+                CategoryDescription = category.CategoryDescription
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.MealCategories.Add(content);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        // GET
+        public IEnumerable<MealCategoryList> GetMealCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.MealCategories
+                    .Select(c => new MealCategoryList
+                    {
+                        CategoryID = c.CategoryID,
+                        CategoryName = c.CategoryName
+                    });
+                return query.ToArray();
+            }
+        }
+
+        // GET ByName
+         
     }
 }
