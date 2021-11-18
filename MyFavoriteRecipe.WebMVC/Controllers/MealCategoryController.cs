592a5f2f6fe86dea64b70f34936d65d6f9aa5876
Ciara Brown <cnbrown2021@gmail.com>
2021-11-17 22:59:42 -0500

    CategoryService Create

--- MyFavoriteRecipe.WebMVC/Controllers/MealCategoryController.cs
+++ MyFavoriteRecipe.WebMVC/Controllers/MealCategoryController.cs
@@ -1,4 +1,6 @@
-﻿using MyFavoriteRecipes.Model;
+﻿using MyFavoriteRecipe.Services;
+using MyFavoriteRecipe.WebMVC.Models;
+using MyFavoriteRecipes.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +12,42 @@ namespace MyFavoriteRecipe.WebMVC.Controllers
     [Authorize]
     public class MealCategoryController : Controller
     {
+        //private ApplicationDbContext _db = new ApplicationDbContext();
+        //private MealCategoryService CreateMealCategoryService()
+        //{
+            //MealCategoryService category = new MealCategoryService();
+          //  return category;
+        //}
         // GET: MealCategory
         public ActionResult Index()
         {
-            var model = new MealCategoryList[0];
-            return View(model);
+            //MealCategoryService mealCategory = CreateMealCategoryService();
+            //var category = mealCategory.GetMealCategories();
+            //return View(category);
+            var service = new MealCategoryService();
+            var category = service.GetMealCategories();
+            return View(category);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
         }
 
         // GET: Create/MealCategory
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(MealCategoryCreate model)
+        public ActionResult Create(MealCategoryCreate category)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(category);
             }
-            return View(model);
+                var service = new MealCategoryService();
+                service.CreateMealCategory(category);
+                return RedirectToAction("Index");
+            
         }
     }
 }
