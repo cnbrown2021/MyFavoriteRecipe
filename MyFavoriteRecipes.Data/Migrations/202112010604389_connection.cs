7d6c7adf0d539257988257a515fca2f48aa3ba80
Ciara Brown <cnbrown2021@gmail.com>
2021-12-01 01:25:41 -0500

    connection

---
+++ MyFavoriteRecipes.Data/Migrations/202112010604389_connection.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFavoriteRecipes.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class connection : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
